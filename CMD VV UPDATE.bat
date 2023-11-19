f0656a38d55a8132517adf5af9b05708cd68c370
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-19 09:48:20 +0700

    Update CMD VV UPDATE.bat

--- CMD VV UPDATE.bat
+++ CMD VV UPDATE.bat
@@ -1,5 +1,5 @@
 :update
-git clone https://github.com/rraapt66/CMD-UPDATE.git
+git clone https://github.com/rraapt66/CMD-VVbysiwat.git
 ping -n 2 127.0.0.1>nul
 echo update Successful
 ping -n 2 127.0.0.1>nul
